e4b58e9f8abbaa7a724d4be920e889e7a9cc3b1b
Dornavan <130479724+dorndickence@users.noreply.github.com>
2023-12-22 17:19:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 
-const { Telegraf } = require('telegraf');
+const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+// Create an instance of the Telegram Bot using your Telegram Bot Token
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
-bot.start((ctx) => ctx.reply('Online!'));
-
-bot.on('text', (ctx) => {
-    var api_url = `http://api.brainshop.ai/get?bid=${process.env.BRAINSHOP_BID}&key=${process.env.BRAINSHOP_KEY}&uid=${ctx.message.chat.id}&msg=${ctx.message.text}`;
-    axios.get(api_url)
-        .then(response => ctx.reply(response.data.cnt))
-        .catch(error => ctx.reply(`${error}`));
+// Trigger to list what the bot can do from the BrainShop
+bot.onText(/\/list/, (msg) => {
+  const chatId = msg.chat.id;
+  const message = 'I can do the following\n1. Chat with you using the BrainShop endpoint\n2. Respond to messages starting with "!".';
+  bot.sendMessage(chatId, message);
 });
 
-bot.launch();
+// Listen for messages starting with "!"
+bot.onText(/^!/, async (msg) => {
+  const chatId = msg.chat.id;
+  const message = msg.text.slice(1); // Remove the "!" prefix
+
+  try {
+    const response = await axios.get(http://api.brainshop.ai/get?bid=${process.env.BRAINSHOP_BOT_ID}&key=${process.env.BRAINSHOP_API_KEY}&uid=${chatId}&msg=${encodeURIComponent(message)});
+    bot.sendMessage(chatId, response.data.cnt);
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(chatId, 'Oops! Something went wrong. Please try again later.');
+  }
+});
